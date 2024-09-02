5b8d2a82d7f9f2dfa340b04bb77930f7450168c8
Tsering Norbu <62776949+tseringn@users.noreply.github.com>
2024-09-01 23:40:34 -0400

    Update friends_controller.rb

--- fisbum-api/app/controllers/api/v1/friends_controller.rb
+++ fisbum-api/app/controllers/api/v1/friends_controller.rb
@@ -9,24 +9,24 @@ class Api::V1::FriendsController < ApplicationController
    friend.destroy
    end
 
-#    def create
-#     u1=User.find(unfriend_params[:user_id])
-#     f1=u1.friends.find do |f|
-#         f.my_friend_id==unfriend_params[:my_friend_id]
-#     end
+    def create
+    u1=User.find(unfriend_params[:user_id])
+    f1=u1.friends.find do |f|
+        f.my_friend_id==unfriend_params[:my_friend_id]
+    end
 
-#     u2=User.find(unfriend_params[:my_friend_id])
-#     f2=u2.friends.find do |f|
-#         f.my_friend_id==unfriend_params[:user_id]
-#     end
-#     f2.destroy
-#     f1.destroy
-#     render json: 'something!'
-#    end
+    u2=User.find(unfriend_params[:my_friend_id])
+    f2=u2.friends.find do |f|
+        f.my_friend_id==unfriend_params[:user_id]
+    end
+    f2.destroy
+    f1.destroy
+    render json: 'something!'
+   end
 
-#    private
+   private
 
-#    def unfriend_params
-#     params.require(:friends).permit(:user_id, :my_friend_id)
-#    end
+   def unfriend_params
+    params.require(:friends).permit(:user_id, :my_friend_id)
+   end
 end
